3d15960a6e1237696ffa3d467ddcd0eb7b830c46
riyanandri <hiddenfoxe01@gmail.com>
2023-08-30 15:34:08 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:qrcode_scanner/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "QR Code Scanner",
       initialRoute: Routes.home,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
